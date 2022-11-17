1eca05adf839d0b6bed44f15dec04de9ed9fb1bb
heshuo <2393097587@qq.com>
2022-11-17 21:17:51 +0800

    master

---
+++ src/Dialog/demo2.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Dialog from '.'
+
+ function Demo2() {
+  return (
+    <div>demo2</div>
+  )
+}
+
+export default Demo2
---
+++ src/Dialog/handleDialog.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function handleDialog() {
+  return (
+    <div>handleDialog</div>
+  )
+}
