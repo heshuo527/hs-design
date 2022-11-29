31160422c9cf4ba1a1b1f8d1bc74997d3b75ac31
heshuo <2393097587@qq.com>
2022-11-29 15:40:42 +0800

    fix: 布局

--- .umirc.ts
+++ .umirc.ts
@@ -1,34 +1,9 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  mode: 'site',
   title: 'hs-design',
   favicon:
     'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
   logo: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
   outputPath: 'docs-dist',
-  // more config: https://d.umijs.org/config
-  description: '用于 Biz Web Dev 前端组件开发。',
-
-  // 打包路径配置
-  base: '/hs-design',
-  publicPath: '/hs-design' + '/', // 打包文件时，引入地址生成 publicPath/xxx.js
-  exportStatic: {}, // 对每隔路由输出html
-  dynamicImport: {}, // 动态导入
-  hash: true, //加hash配置，清除缓存
-  manifest: {
-    // 内部发布系统规定必须配置
-    fileName: 'manifest.json',
-  },
-
-  // 多国语顺序
-  locales: [
-    ['en-US', 'English'],
-    ['zh-CN', '中文'],
-  ],
-
-  // 主题
-  theme: {
-    '@primary': '#16c35f',
-  },
 });
