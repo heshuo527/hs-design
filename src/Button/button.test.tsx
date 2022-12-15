428130483546082ed0e587ef77b740a8eb1620e5
heshuo <2393097587@qq.com>
2022-12-15 23:42:38 +0800

    feat: 发布npm

--- src/Button/button.test.tsx
+++ src/Button/button.test.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Button from '../Button';
 
-test('测试 Button 组件', () => {
+describe('测试 Button 组件', () => {
   it('default Button', () => {
     const wrapper = render(<Button>Nice</Button>);
     const element = wrapper.getByText('Nice');
-    expect(element.tagName).toEqual('BUTTON');
     expect(element);
+    expect(element.tagName).toEqual('BUTTON');
+    expect(element).toHaveClass('hs-button');
   });
 });
