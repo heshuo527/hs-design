969e11e26eed0ef5bff5e3c67608b7023fca5b15
heshuo <2393097587@qq.com>
2022-12-28 22:27:49 +0800

    feat: button test

--- src/Button/button.test.tsx
+++ src/Button/button.test.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Button, { ButtonProps } from './index';
 
 const defaultProps = {
+  disabled: false,
   onClick: jest.fn(),
-};
-
-const classProps: ButtonProps = {
-  type: 'default',
-  size: 'medium',
-  className: 'hs-design',
 };
 
 const disabledProps: ButtonProps = {
@@ -20,8 +16,9 @@ const disabledProps: ButtonProps = {
 describe('test Button component', () => {
   it('should render the correct default button', () => {
     // render 方法返回一个包裹对象, 对象包裹一些对DOM的查询和获取方法
-    const wrapper = render(<Button {...defaultProps}>hello</Button>);
-    const element = wrapper.queryByText('hello') as HTMLButtonElement;
+    const wrapper = render(<Button {...defaultProps}>hs</Button>);
+    const element = wrapper.getByText('hs') as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
     // 断言他是一个button组件
     expect(element.tagName).toEqual('BUTTON');
     // 元素上是否存在disabled禁用属性
@@ -31,13 +28,15 @@ describe('test Button component', () => {
     expect(defaultProps.onClick).toHaveBeenCalled();
   });
   it('should render the correct button className', () => {
-    const wrapper = render(<Button {...classProps}>hs</Button>);
-    const element = wrapper.queryByText('hs');
-    // expect(element).toHaveClass('hs-design hs-design-default')
+    const wrapper = render(<Button>hs</Button>);
+    const element = wrapper.getByText('hs');
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass('hs-button hs-button-default hs-button-medium');
   });
   it('should render disabled button when disabled set to true', () => {
-    const wrapper = render(<Button {...disabledProps}>Nice</Button>);
-    const element = wrapper.getByText('Nice') as HTMLButtonElement;
+    const wrapper = render(<Button {...disabledProps}>hs</Button>);
+    const element = wrapper.getByText('hs') as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
     expect(element.disabled).toBeTruthy();
     fireEvent.click(element);
     expect(disabledProps.onClick).not.toHaveBeenCalled();
